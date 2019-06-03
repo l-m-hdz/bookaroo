67d5309c13e3b57ecd3c24e6ea84857b2299df2e
Leslie Hernandez <lehernandez510@gmail.com>
2019-06-03 22:10:33 +0000

    Everything

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 // JavaScript File
-$("#search-button").click(function(){
+/* global $ */
+$(document).ready(function() {
+$("#titles").click(function(){
+    console.log("Horror")
    $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes/tcSMCwAAQBAJ",
+        url: "https://www.googleapis.com/books/v1/volumes?q=harrypotter",
         method: "GET",
         success: function(response) {
-          console.log('SUCESSSS');
+          console.log(response.items[0].volumeInfo.title);
+          console.log(response.items[0].volumeInfo.authors[0]);                                                
+   $("#gen1").html(response.items[0].volumeInfo.title);
+       
+   
         
-        }
+    }
+   });
+});
+});
+$(document).ready(function() {
+$("#titles").click(function(){
+    console.log("Sci-FI")
+   $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes/DKcWE3WXoj8C",
+        method: "GET",
+        success: function(response) {
+          console.log(response.items[0].volumeInfo.title);
+          console.log(response.items[0].volumeInfo.authors[0]);                                                
+   $("#gen2").html(response.items[0].volumeInfo.title);
+       
+   
+        
+    }
+   });
 });
 });
